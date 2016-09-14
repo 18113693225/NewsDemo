7185bc33d62b094112823c53f009a663a414f9de
lijun <309150945@qq.com>
2016-09-14 17:20:28 +0800

    细节修改

--- app/src/main/java/com/apps/android/news/news/ui/activity/IntroActivity.java
+++ app/src/main/java/com/apps/android/news/news/ui/activity/IntroActivity.java
@@ -39,34 +39,18 @@ public class IntroActivity extends BaseActivity {
     }
 
     private void getAllTable() {
-        Base base = new Base();
-        base.userId = "dd";
-        Call<Channels> tabCall = mTabService.AllTable(ParamUtils.initParam("news\\actions\\mobile\\actions\\channels.xml", JsonUtils.get().toJson(base)));
+        Call<Channels> tabCall = mTabService.AllTable(ParamUtils.initParam("news\\actions\\mobile\\actions\\channels.xml", null));
         mNetworkUtils = NetworkUtils.create(new MessageCallback<Channels>(this) {
 
             @Override
             public void respondSuccess(Channels data) {
                 super.respondSuccess(data);
-                String id = data.channels.get(0).id;
-                if(data.channels!=null)cDao.initChannels(data.channels);
-                JSONArray channels = cDao.getChannels();
-
+                if (data.channels != null) {
+                    cDao.initChannels(data.channels);
+                }
+              
             }
 
-            @Override
-            public void startRequest() {
-                super.startRequest();
-            }
-
-            @Override
-            public void endRequest() {
-                super.endRequest();
-            }
-
-            @Override
-            public void error(ErrorModel errorModel) {
-                super.error(errorModel);
-            }
         });
         mNetworkUtils.enqueue(tabCall);
     }
